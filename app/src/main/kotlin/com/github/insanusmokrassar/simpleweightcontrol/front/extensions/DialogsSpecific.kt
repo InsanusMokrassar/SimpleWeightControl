a9b724ebf1a81a609fbf207cc1938bad473c3a5c
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-11-24 16:55:54 +0800

    try to replace pickers by buttons

--- app/src/main/kotlin/com/github/insanusmokrassar/simpleweightcontrol/front/extensions/DialogsSpecific.kt
+++ app/src/main/kotlin/com/github/insanusmokrassar/simpleweightcontrol/front/extensions/DialogsSpecific.kt
@@ -1,13 +1,12 @@
 package com.github.insanusmokrassar.simpleweightcontrol.front.extensions
 
 import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Context
 import android.os.Build
 import android.view.LayoutInflater
-import android.widget.DatePicker
-import android.widget.EditText
-import android.widget.TimePicker
-import android.widget.Toast
+import android.widget.*
 import com.github.insanusmokrassar.simpleweightcontrol.R
 import com.github.insanusmokrassar.simpleweightcontrol.common.models.WeightData
 import java.util.*
@@ -26,8 +25,8 @@ fun Context.createEditWeightDialog(
     )
 
     val weightEditText = view.findViewById<EditText>(R.id.weightEditText)
-    val weightDatePicker = view.findViewById<DatePicker>(R.id.weightDatePicker)
-    val weightTimePicker = view.findViewById<TimePicker>(R.id.weightTimePicker)
+    val weightDateButton = view.findViewById<Button>(R.id.weightDateButton)
+    val weightTimeButton = view.findViewById<Button>(R.id.weightTimeButton)
 
     val calendar: Calendar = Calendar.getInstance()
     weight ?. let {
@@ -46,18 +45,44 @@ fun Context.createEditWeightDialog(
         calendar.time = Date(System.currentTimeMillis())
     }()
 
-    weightDatePicker.updateDate(calendar.year(), calendar.month(), calendar.day())
-
-    weightTimePicker.setIs24HourView(true)
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        weightTimePicker.hour = calendar.hourOfDay()
-        weightTimePicker.minute = calendar.minutes()
-    } else {
-        weightTimePicker.currentHour = calendar.hourOfDay()
-        weightTimePicker.currentMinute = calendar.minutes()
+    weightDateButton.setOnClickListener {
+        DatePickerDialog(
+                this,
+                {
+                    _, year, monthOfYear, dayOfMonth ->
+                    calendar.set(year, monthOfYear, dayOfMonth)
+                },
+                calendar.year(),
+                calendar.month(),
+                calendar.day()
+        ).show()
     }
 
-    weightDatePicker.maxDate = System.currentTimeMillis()
+    weightTimeButton.setOnClickListener {
+        TimePickerDialog(
+                this,
+                { _: TimePicker, hours: Int, minutes: Int ->
+                    calendar.set(Calendar.HOUR_OF_DAY, hours)
+                    calendar.set(Calendar.MINUTE, minutes)
+                },
+                calendar.hourOfDay(),
+                calendar.minutes(),
+                true
+        ).show()
+    }
+
+//    weightDatePicker.updateDate(calendar.year(), calendar.month(), calendar.day())
+//
+//    weightTimePicker.setIs24HourView(true)
+//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//        weightTimePicker.hour = calendar.hourOfDay()
+//        weightTimePicker.minute = calendar.minutes()
+//    } else {
+//        weightTimePicker.currentHour = calendar.hourOfDay()
+//        weightTimePicker.currentMinute = calendar.minutes()
+//    }
+//
+//    weightDatePicker.maxDate = System.currentTimeMillis()
 
     builder.setView(view)
 
@@ -67,7 +92,7 @@ fun Context.createEditWeightDialog(
             {
                 dialogInterface, i ->
                 wasError = try {
-                    val actualTime = getCalendar(weightDatePicker, weightTimePicker).timeInMillis
+                    val actualTime = calendar.timeInMillis
                     if (actualTime > System.currentTimeMillis()) {
                         Toast.makeText(this, getString(R.string.typedWrongTime), Toast.LENGTH_LONG).show()
                         true
