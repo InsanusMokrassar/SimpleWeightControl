59b4371b4688a8fd298b57c5c346560756f33055
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-11-25 18:19:19 +0800

    fix millisInDay

--- app/src/main/kotlin/com/github/insanusmokrassar/simpleweightcontrol/back/utils/database/WeightHelper.kt
+++ app/src/main/kotlin/com/github/insanusmokrassar/simpleweightcontrol/back/utils/database/WeightHelper.kt
@@ -4,10 +4,8 @@ import android.content.Context
 import com.github.insanusmokrassar.simpleweightcontrol.R
 import com.github.insanusmokrassar.simpleweightcontrol.back.utils.database.common.MutableListDatabase
 import com.github.insanusmokrassar.simpleweightcontrol.common.models.WeightData
-import java.util.*
-import kotlin.collections.HashSet
 
-private val millisInDay: Long = 48*60*60*1000
+private val millisInDay: Long = 24*60*60*1000
 
 private var weightHelper: WeightHelper? = null
 
